82f39a59c4c652347fe9ba2a8d5fca399e8098cc
hayden <olex1216@gmail.com>
2018-10-31 15:21:42 +0800

    对应commit忘记密码重置功能实现完毕，并进行了必要的测试。对应6-11,6-12

--- mxonline3/urls.py
+++ mxonline3/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.views.generic import TemplateView
 import xadmin
 from django.urls import path, include, re_path
-from users.views import LoginView, RegisterView, ActiveUserView
+from users.views import LoginView, RegisterView, ActiveUserView, ForgetPwdView, ResetView, ModifyPwdView
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
@@ -34,5 +34,15 @@ urlpatterns = [
     # 验证码url
     path("captcha/", include('captcha.urls')),
 
-]    # 激活用户url
-    re_path('active/(?P<active_code>.*)/', ActiveUserView.as_view(), name= "user_active")
+    # 激活用户url
+    re_path('active/(?P<active_code>.*)/', ActiveUserView.as_view(), name="user_active"),
+
+    # 忘记密码
+    path('forget/', ForgetPwdView.as_view(), name="forget_pwd"),
+
+    # 重置密码urlc ：用来接收来自邮箱的重置链接
+    re_path('reset/(?P<active_code>.*)/', ResetView.as_view(), name="reset_pwd"),
+
+    # 修改密码url; 用于passwordreset页面提交表单
+    path('modify_pwd/', ModifyPwdView.as_view(), name="modify_pwd"),
+]
