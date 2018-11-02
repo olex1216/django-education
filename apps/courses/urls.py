97426341b3f33a4d1f9e003d6a6e1cb245449afd
hayden <olex1216@gmail.com>
2018-11-02 10:30:39 +0800

    8-7完成相关课程推荐功能，取出相关课程去除本身。课程评论login require鉴权添加。登录页面重定向回登录前浏览页面

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -3,9 +3,9 @@
 __author__ = 'hayden'
 __date__ = '2018/11/1 16:35'
 
-from .views import CourseListView,CourseDetailView,CourseInfoView
+from .views import CourseListView, CourseDetailView, CourseInfoView, CommentsView, AddCommentsView
 
-from django.urls import path,re_path
+from django.urls import path, re_path
 
 app_name = "courses"
 
@@ -16,5 +16,9 @@ urlpatterns = [
     re_path('detail/(?P<course_id>\d+)/', CourseDetailView.as_view(), name="course_detail"),
     # 课程章节信息页
     re_path('info/(?P<course_id>\d+)/', CourseInfoView.as_view(), name="course_info"),
+    # 课程章节信息页
+    re_path('comments/(?P<course_id>\d+)/', CommentsView.as_view(), name="course_comments"),
 
+    # 添加课程评论,已经把参数放到post当中了
+    path('add_comment/', AddCommentsView.as_view(), name="add_comment"),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import View
 from .models import Course,CourseResource
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
-from operation.models import UserFavorite
+from operation.models import UserFavorite,CourseComments,UserCourse
 
 
 class CourseListView(View):
@@ -74,14 +74,69 @@ class CourseDetailView(View):
 
 
 # 处理课程章节信息页面的view
+class CourseInfoView(LoginRequiredMixin,View):
+        login_url = '/login/'
+        redirect_field_name = 'next'
 
-class CourseInfoView(View):
+        def get(self, request, course_id):
+            # 此处的id为表默认为我们添加的值。
+            course = Course.objects.get(id=int(course_id))
+
+            # 查询用户是否开始学习了该课，如果还未学习则，加入用户课程表
+            user_courses = UserCourse.objects.filter(user=request.user, course=course)
+            if not user_courses:
+                user_course = UserCourse(user=request.user, course=course)
+                user_course.save()
+            # 查询课程资源
+            all_resources = CourseResource.objects.filter(course=course)
+            # 选出学了这门课的学生关系
+            user_courses = UserCourse.objects.filter(course=course)
+            # 从关系中取出user_id
+            user_ids = [user_course.user_id for user_course in user_courses]
+            # 这些用户学了的课程,外键会自动有id，取到字段
+            all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+            # 取出所有课程id
+            course_ids = [user_course.course_id for user_course in all_user_courses]
+            # 获取学过该课程用户学过的其他课程
+            relate_courses = Course.objects.filter(id__in=course_ids).order_by("-click_nums").exclude(id=course.id)[:4]
+            # 是否收藏课程
+            return render(request, "course-video.html", {
+                "course": course,
+                "all_resources": all_resources,
+                "relate_courses": relate_courses,
+            })
+
+
+class CommentsView(View):
     def get(self, request, course_id):
         # 此处的id为表默认为我们添加的值。
         course = Course.objects.get(id=int(course_id))
         all_resources = CourseResource.objects.filter(course=course)
-        # 是否收藏课程
-        return render(request, "course-video.html", {
+        all_comments = CourseComments.objects.all()
+        return render(request, "course-comment.html", {
             "course": course,
             "all_resources": all_resources,
+            "all_comments":all_comments,
         })
+
+# ajax方式添加评论
+class AddCommentsView(View):
+    def post(self, request):
+        if not request.user.is_authenticated:
+            # 未登录时返回json提示未登录，跳转到登录页面是在ajax中做的
+            return HttpResponse('{"status":"fail", "msg":"用户未登录"}', content_type='application/json')
+        course_id = request.POST.get("course_id", 0)
+        comments = request.POST.get("comments", "")
+        if int(course_id) > 0 and comments:
+            course_comments = CourseComments()
+            # get只能取出一条数据，如果有多条抛出异常。没有数据也抛异常
+            # filter取一个列表出来，queryset。没有数据返回空的queryset不会抛异常
+            course = Course.objects.get(id = int(course_id))
+            # 外键存入要存入对象
+            course_comments.course = course
+            course_comments.comments = comments
+            course_comments.user = request.user
+            course_comments.save()
+            return HttpResponse('{"status":"success", "msg":"评论成功"}', content_type='application/json')
+        else:
+            return HttpResponse('{"status":"fail", "msg":"评论失败"}', content_type='application/json')
